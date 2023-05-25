41b1fb84ef95da5255bb4bf0bce16e160fed5b9e
milan <milan@milans-MacBook-Pro.local>
2023-05-25 11:19:15 +0530

    ceiling of a number

---
+++ BinarySearch/ceilingOfNumber.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{2, 3, 5, 9, 14, 16, 18}
+
+	target := 15
+	fmt.Println(ceilingOfNumber(arr, target))
+}
+
+func ceilingOfNumber(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+
+		if target > arr[mid] {
+			start = mid + 1
+		} else if target < arr[mid] {
+			end = mid - 1
+		} else {
+			return arr[mid]
+		}
+
+	}
+	return arr[start]
+}
