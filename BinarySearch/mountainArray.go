72fe28b3d611f7077960a639bc0d9972033be0e7
milan <milan@milans-MacBook-Pro.local>
2023-06-05 10:34:38 +0530

    mountain array

---
+++ BinarySearch/mountainArray.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{0, 10, 5, 2}
+	fmt.Println(peakIndex(arr))
+}
+func peakIndex(arr []int) int {
+	start := 0
+	end := len(arr) - 1
+
+	for start < end {
+		mid := start + (end-start)/2
+
+		if arr[mid] > arr[mid+1] {
+			//you are in decreasing part
+			//this might be answer but look left
+			end = mid
+		} else {
+			//asc part of array
+			start = mid + 1
+		}
+	}
+	return arr[start]
+}
